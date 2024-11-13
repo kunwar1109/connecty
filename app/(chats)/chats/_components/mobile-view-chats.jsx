65fe0890ec374fab423bb8f9b2337b5086d67b31
kunwar1109 <161008465+kunwar1109@users.noreply.github.com>
2024-11-13 12:13:36 +0530

    reverting back

--- app/(chats)/chats/_components/mobile-view-chats.jsx
+++
@@ -1,6 +0,0 @@
-export const MobileViewChats = () => {
-  //fetch all chats for user
-  return (
-    <div className="block w-full h-screen md:hidden">Mobile Chat View</div>
-  );
-};
--- app/(chats)/chats/page.jsx
+++ app/(chats)/chats/page.jsx
@@ -1,13 +1,9 @@
-import { MobileViewChats } from "./_components/mobile-view-chats";
-
 export default function ChatPage() {
   return (
     <>
       <div className="hidden md:h-screen md:w-full md:flex md:justify-center md:items-center">
         Chat Page
       </div>
-      {/* Mobile All Chats List */}
-      <MobileViewChats />
     </>
   );
 }
